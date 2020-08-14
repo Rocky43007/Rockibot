94b95ac55b21f4a062dbffcc3aac86ff12f6b398
Arnab Chakraborty <arnabfromtks@gmail.com>
2020-08-14 15:43:26 +0300

    !stats looks better and has more information.

---
+++ bot.js
@@ -0,0 +1,155 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { token, default_prefix } = require('./config.json');
+const Canvas = require('canvas');
+const Keyv = require('keyv');
+const prefixes = new Keyv('sqlite:///home/ricky/DiscordBotTest/db.sqlite');
+const globalPrefix = default_prefix;
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+	console.log('Ready!');
+	console.log(`Server Count: ${client.guilds.cache.size}`);
+	client.user.setActivity('with !help | https://discord.gg/Ju2gSCY');
+});
+
+client.on('message', async message => {
+	if (message.author.bot) return;
+
+	let args;
+	// handle messages in a guild
+	if (message.guild) {
+		let prefix;
+
+		if (message.content.startsWith(globalPrefix)) {
+			prefix = globalPrefix;
+		}
+		else {
+			// check the guild-level prefix
+			const guildPrefix = await prefixes.get(message.guild.id);
+			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
+		}
+
+		// if we found a prefix, setup args; otherwise, this isn't a command
+		if (!prefix) return;
+		args = message.content.slice(prefix.length).trim().split(/\s+/);
+	}
+	else {
+		// handle DMs
+		const slice = message.content.startsWith(globalPrefix) ? globalPrefix.length : 0;
+		args = message.content.slice(slice).split(/\s+/);
+	}
+
+	// get the first space-delimited argument after the prefix as the command
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${globalPrefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+	let fontSize = 70;
+
+	do {
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	return ctx.font;
+};
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
+});
+
+client.on('message', message => {
+	if (message.content === '!join') {
+		client.emit('guildMemberAdd', message.member);
+	}
+});
+client.login(token);
--- commands/stats.js
+++ commands/stats.js
@@ -1,8 +1,77 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const moment = require('moment');
+const fs = require('fs');
+const os = require('os');
+const { datastore } = require('googleapis/build/src/apis/datastore');
+
 module.exports = {
 	name: 'stats',
 	description: 'Server Stats',
 	guildOnly: true,
-	execute(message, args) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	execute: async (message) => {
+		const promises = [
+			client.shard.fetchClientValues('guilds.cache.size'),
+			client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+			client.shard.fetchClientValues('channels.cache.size'),
+			client.shard.broadcastEval(`
+			[
+				this.shard.id,
+			]
+			`),
+		];
+
+
+		return Promise.all(promises)
+			.then(results => {
+				const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+				const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+				const totalChannels = results[2].reduce((acc, channelCount) => acc + channelCount, 0);
+				const totalShards = results[3].reduce((acc, shardCount) => acc + shardCount, 0);
+				const NumberOfFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')).length;
+				const usedMemory = os.totalmem() - os.freemem(), totalMemory = os.totalmem();
+				const getpercentage = ((usedMemory / totalMemory) * 100).toFixed(2) + '%';
+				const uptime = process.uptime();
+				console.log('Uptime raw:', uptime);
+				const date = new Date(uptime * 1000);
+				const days = date.getUTCDate() - 1,
+					hours = date.getUTCHours(),
+					minutes = date.getUTCMinutes(),
+					seconds = date.getUTCSeconds(),
+					milliseconds = date.getUTCMilliseconds();
+				const segments = [];
+				if (days > 0) segments.push(days + ' day' + ((days == 1) ? '' : 's'));
+				if (hours > 0) segments.push(hours + ' hour' + ((hours == 1) ? '' : 's'));
+				if (minutes > 0) segments.push(minutes + ' minute' + ((minutes == 1) ? '' : 's'));
+				if (seconds > 0) segments.push(seconds + ' second' + ((seconds == 1) ? '' : 's'));
+				const dateString = segments.join(', ');
+				const embed = new Discord.MessageEmbed()
+					.setColor('#00FFFF')
+					.setTitle('Rocky\'s Modular Bot Stats')
+					.addField('Servers:', `${totalGuilds}`)
+					.addField('Users:', `${totalMembers}`)
+					.addField('Channels:', `${totalChannels}`)
+					.addField('Shard:', `${totalShards}`)
+					.addField('Creator:', 'Rocky43007#7727')
+					.addField('Version:', '0.0.1-Alpha')
+					.addField('Number of Commands:', `${NumberOfFiles}`)
+					.addField('Memory Usage:', `${getpercentage} (${(usedMemory / Math.pow(1024, 3)).toFixed(2)} GB)`)
+					.addField('Uptime:', `${dateString}`)
+					.addField('Discord.js Version:', 'v12.2.0')
+					.addField('Operating System:', 'Ubuntu 20.04 LTS')
+					.addField('Kernel:', `${os.release}`)
+					.setFooter(moment().calendar(null, {
+						sameDay: '[Today], LT',
+						nextDay: '[Tomorrow]',
+						nextWeek: 'dddd',
+						lastDay: '[Yesterday], LT',
+						lastWeek: '[Last] dddd, LT',
+						sameElse: 'DD/MM/YYYY',
+					}));
+
+				return message.channel.send(embed);
+			})
+
+			.catch(console.error);
 	},
 };
