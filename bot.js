16b4dc5cbd6b6e4ee5a86f920c551a7138987221
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-10 08:56:16 +0300

    Change of index stuff.

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const { CommandoClient } = require('discord.js-commando');
 const discord = require('discord.js');
 const path = require('path');
+const config = requre('./config.json');
 const Keyv = require('keyv');
 const KeyvProvider = require('commando-provider-keyv');
 const Canvas = require('canvas');
@@ -113,4 +114,4 @@ client.on('guildMemberAdd', async member => {
 	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
-client.login(process.env.TOKEN);
+client.login(config.token);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const config = require('./config.json');
-const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
+const manager = new ShardingManager('./bot.js', { token: config.token });
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
