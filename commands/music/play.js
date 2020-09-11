b957e90c978fa624a17486fccf41c5e3bd663fa4
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-11 14:29:24 +0300

    Addition of music again.

---
+++ commands/music/play.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord.js-commando');
+const queues = {};
+const ytdl = require('ytdl-core');
+const search = require('youtube-search');
+const Spotify = require('')
+
+module.exports = class MusicPlay extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'play',
+			aliases: ['p'],
+			group: 'music',
+			memberName: 'play',
+			description: 'Allows bot to play music for the user in a voice channel.',
+			args: [
+				{
+					key: 'song',
+					prompt: 'What song would you like to listen to?',
+					type: 'string',
+				},
+			],
+			guildOnly: true,
+		});
+	}
+	async run(message, { song }) {
+		if (message.channel.type === 'dm') return;
+
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			return message.reply('Please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl(song, { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+
+			dispatcher.on('finish');
+			setTimeout(10000);
+		});
+	}
+};
