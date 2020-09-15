333bfac6fded91394dcaaafad80314765f5b9881
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-15 21:14:06 +0300

    New Database stuff without Keyv

--- commands/moderation/logs.js
+++ commands/moderation/logs.js
@@ -1,6 +1,11 @@
 const { Command } = require('discord.js-commando');
 const Keyv = require('keyv');
 const logsdb = new Keyv(process.env.DATABASE_URL, { table: 'modlogs' });
+const { Client2 } = require('pg');
+
+
+
+
 
 module.exports = class modlogs extends Command {
 	constructor(client) {
@@ -22,7 +27,22 @@ module.exports = class modlogs extends Command {
 		});
 	}
 	async run(message, { logs }) {
-		console.log(process.env.DATABASE_URL);
+		const client3 = new Client2({
+			connectionString: process.env.DATABASE_URL,
+			ssl: {
+			  rejectUnauthorized: false
+			}
+		  });
+		  
+		  client3.connect();
+		  
+		  client3.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+			if (err) throw err;
+			for (let row of res.rows) {
+			  console.log(JSON.stringify(row));
+			}
+			client3.end();
+		  });
 		await logsdb.set(message.guild.id, logs).then(
 			message.channel.send(`Successfully set mod log to \`${logs}\``),
 		);
