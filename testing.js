32c7afd74a9ca598473bbc9ede615cfa13607cab
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-20 18:07:04 +0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,7 +1,17 @@
 const { client } = require('discord.js');
-const path = require('path');
+const fs = require('fs');
+const Discord = require('discord.js');
 const { token } = process.env.TOKEN
+const prefix = '!';
 
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -9,6 +19,21 @@ client.once('ready', () => {
 client.once('ready', () => {
 	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
 	client.user.setActivity('with !help | discord.gg/Ju2gSCY');
+});
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
 client.login(token);
