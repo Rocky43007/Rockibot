a84fe92a7ee03b63f1033c308d5e5483e01ef2d8
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-20 19:12:23 +0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,9 +1,9 @@
-const { client } = require('discord.js');
-const fs = require('fs');
 const Discord = require('discord.js');
+const fs = require('fs');
 const { token } = process.env.TOKEN;
-const prefix = '!';
+const prefix = process.env.DEFPREFIX;
 
+const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -11,12 +11,12 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
-};
+}
 
 client.once('ready', () => {
-	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-	client.user.setActivity('with !help | discord.gg/Ju2gSCY');
+	console.log('Ready!');
 });
+
 client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
