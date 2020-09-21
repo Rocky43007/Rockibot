b50be92f7033cfc459cdeff4fb868370d2ac5849
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-21 09:22:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./testing.js', { token: process.env.TOKEN });
+const manager = new ShardingManager('./bot.js', { token: process.env.TOKEN });
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
