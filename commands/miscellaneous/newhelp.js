49a8e6c4ec6ed258e2a1b3a06273359ab9b6c537
Rocky43007 <Rocky43007@users.noreply.github.com>
2020-09-24 21:19:53 +0300

    Update newhelp.js

--- commands/miscellaneous/newhelp.js
+++ commands/miscellaneous/newhelp.js
@@ -19,8 +19,15 @@ module.exports = class help2 extends Command {
 		});
 	}
 	run(message, page) {
-		if (page === 1) {
-			const basic = new Discord.MessageEmbed()
+		const home = new Discord.MessageEmbed()
+		.setColor('#03d3fc')
+		.setTitle('Rockibot | Help')
+		.setDescription('Use `!help [page]` to switch pages!')
+		.addField('Page 1: General Commands', 'These commands are given to everyone, and is the module that every user gets when they use the bot.')
+		.addField('Page 2: Moderation Commands', 'These commands are for moderating servers, such as `!ban`, `!kick` and more!')
+		.addField('Page 3: Music Commands', 'These commands are for the music aspect of the bot. Here, you can find information of `!play`, `!join` and more!')
+		.addField('Page 4: Suggestion Commands', 'These commands relate to suggestions! Here, you can find information about `!suggest`, `!suggest-channel` and more!')
+		const basic = new Discord.MessageEmbed()
 			.setColor('#4e03fc')
 			.setTitle('Rockibot | General Commands')
 			.setDescription('Please remove the `<>` when using the commands!')
@@ -30,10 +37,17 @@ module.exports = class help2 extends Command {
 			.addField('status', 'Used to check if the bot is working or not.')
 			.addField('stats', 'Nerdy stats for those who want to see who made the bot, and how much memory is being used, and what version the bot is.')
 			.addField('invite', 'Allows other users to invite the bot to their own server.')
-			message.channel.send(basic);
-		}
-		if (page === 2) {
-			const mod = new Discord.MessageEmbed()
+		const suggestions = new Discord.MessageEmbed()
+			.setColor('#2e77b8')
+			.setTitle('Rockibot | Suggestions Commands')
+			.setDescription('Please remove the `<>` when using the commands!')
+			.addField('suggest-channel', 'Sets the channel where the suggestions will be found. Usage: `!suggest-channel <Suggestion Channel Name without #>`.')
+			.addField('suggestion', 'Sends a suggestion to the suggestion channel. Usage: `!suggestion <Suggestion>`.')
+			.addField('approve', 'Says that the suggestion is approved. Usage: `!approve <Suggestion Message ID> <Comments>`.')
+			.addField('consider', 'Says that the suggestion is considered. Usage: `!consider <Suggestion Message ID> <Comments>`.')
+			.addField('implement', 'Says that the suggestion is implemented. Usage: `!implement <Suggestion Message ID> <Comments>`.')
+			.addField('deny', 'Says that the suggestion is denied. Usage: `!deny <Suggestion Message ID> <Comments>`.')
+		const mod = new Discord.MessageEmbed()
 			.setColor('#ff2050')
 			.setTitle('Rockibot | Moderation Commands')
 			.setDescription('Please remove the `<>` when using the commands!')
@@ -46,10 +60,7 @@ module.exports = class help2 extends Command {
 			.addField('warn', 'Allows staff to warn a member. Usage: `!warn <Mention User or User ID> <Reason>`.')
 			.addField('unwarn', 'Allows staff to remove a warn from a member. Usage: `!unwarn <Mention User or User ID> <Reason>`.')
 			.addField('warns', 'Allows staff to see how many warns a user has. Usage: `!warns <Mention User or User ID>`.')
-			message.channel.send(mod);
-		}
-		if (page === 3) {
-			const music = new Discord.MessageEmbed()
+		const music = new Discord.MessageEmbed()
 			.setColor('#2e77b8')
 			.setTitle('Rockibot | Music Commands')
 			.setDescription('Please remove the `<>` when using the commands!')
@@ -57,29 +68,22 @@ module.exports = class help2 extends Command {
 			.addField('leave', 'Makes the bot leave the voice channel it was in.')
 			.addField('play', 'Plays Youtube links in the voice channel. Usage: !play <Youtube link>.')
 			.addField('pause', 'Pauses the music being played.')
+		if (page = 1) {
+			message.channel.send(basic);
+		}
+		if (page = 2) {
+			message.channel.send(mod);
+		}
+		if (page = 3) {
 			message.channel.send(music);
 		}
-		if (page === 4) {
-			const suggestions = new Discord.MessageEmbed()
-			.setColor('#2e77b8')
-			.setTitle('Rockibot | Suggestions Commands')
-			.setDescription('Please remove the `<>` when using the commands!')
-			.addField('suggest-channel', 'Sets the channel where the suggestions will be found. Usage: `!suggest-channel <Suggestion Channel Name without #>`.')
-			.addField('suggestion', 'Sends a suggestion to the suggestion channel. Usage: `!suggestion <Suggestion>`.')
-			.addField('approve', 'Says that the suggestion is approved. Usage: `!approve <Suggestion Message ID> <Comments>`.')
-			.addField('consider', 'Says that the suggestion is considered. Usage: `!consider <Suggestion Message ID> <Comments>`.')
-			.addField('implement', 'Says that the suggestion is implemented. Usage: `!implement <Suggestion Message ID> <Comments>`.')
-			.addField('deny', 'Says that the suggestion is denied. Usage: `!deny <Suggestion Message ID> <Comments>`.')
+		if (page = 4) {
 			message.channel.send(suggestions);
 		}
-		const home = new Discord.MessageEmbed()
-		.setColor('#03d3fc')
-		.setTitle('Rockibot | Help')
-		.setDescription('Use `!help [page]` to switch pages!')
-		.addField('Page 1: General Commands', 'These commands are given to everyone, and is the module that every user gets when they use the bot.')
-		.addField('Page 2: Moderation Commands', 'These commands are for moderating servers, such as `!ban`, `!kick` and more!')
-		.addField('Page 3: Music Commands', 'These commands are for the music aspect of the bot. Here, you can find information of `!play`, `!join` and more!')
-		.addField('Page 4: Suggestion Commands', 'These commands relate to suggestions! Here, you can find information about `!suggest`, `!suggest-channel` and more!')
-		message.channel.send(home);
+		else {
+			message.channel.send(home);
+		}
+		
+
 	}
 };
