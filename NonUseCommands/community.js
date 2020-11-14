55c7e19ab26691a2e590297953ff38e704b4170c
Rocky43007 <arnabfromtks@gmail.com>
2020-11-14 03:01:37 -0800

    Command for community server

---
+++ NonUseCommands/community.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'beep',
+	description: 'Beep makes Boop!',
+	execute(message, args) {
+		message.author.send("Here is our support server! https://discord.gg/pjbRn78ayW")
+	},
+};
