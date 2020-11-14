e63505832a7b33b3b9dd73bf68f455a964135ad2
Rocky43007 <arnabfromtks@gmail.com>
2020-11-14 03:12:13 -0800

    Revert "Command for community server"
    This reverts commit 55c7e19ab26691a2e590297953ff38e704b4170c.

--- NonUseCommands/community.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'beep',
-	description: 'Beep makes Boop!',
-	execute(message, args) {
-		message.author.send("Here is our support server! https://discord.gg/pjbRn78ayW")
-	},
-};
