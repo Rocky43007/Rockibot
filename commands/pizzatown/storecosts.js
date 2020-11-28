3df8da8f0fe91550b0a868534d9bccdf02663930
Rocky43007 <arnabfromtks@gmail.com>
2020-11-28 04:15:56 -0800

    storecosts is back

---
+++ commands/pizzatown/storecosts.js
@@ -0,0 +1,39 @@
+const { Command } = require('discord.js-commando');
+const path = require('path');
+const mconfig = require(path.join(__dirname, 'mconfig.json'));
+const Discord = require("discord.js");
+const {Seller} = require("./models/Sellers");
+
+module.exports = class gstart extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'storecosts',
+			group: 'pizzatown',
+			memberName: 'storecosts',
+			description: 'Displays the costs of stores.',
+			guildOnly: true,
+		});
+	}
+	async run(message, {type}) {
+        Seller.findOne({discord_id:message.author.id}).then(async user => {
+			const urbancost=Math.round(user.stores.filter(store => store.profitmultiplier === 5).length * (1000 * (user.stores.filter(store => store.profitmultiplier === 5).length / 5)) + 5000)
+			const suburbancost=Math.round(user.stores.filter(store => store.profitmultiplier === 3).length * (1000 * (user.stores.filter(store => store.profitmultiplier === 3).length / 5)) + 3000)
+			const standcost=Math.round(user.stores.filter(store => store.profitmultiplier === 2).length * (1000 * (user.stores.filter(store => store.profitmultiplier === 2).length / 5)) + 1000)
+			
+            const embed=new Discord.MessageEmbed()
+            .setColor("#c826fd")
+            .setTitle("Store costs.")
+            .addFields(
+                {name:"Urban costs", value:urbancost},
+                {name:"Suburban costs", value:suburbancost},
+                {name:"Stand costs", value:standcost},
+            )
+        }).catch(() => {
+	const embed = new Discord.MessageEmbed()
+	.setColor('#c22419')
+	.setTitle("You are not a seller!")
+        return message.channel.send(embed);
+        })
+	}
+};
+
