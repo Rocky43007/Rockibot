78c08b856ad0fdd22cac9e93416a8ad0612e2e46
Rocky43007 <arnabfromtks@gmail.com>
2020-12-17 10:11:50 -0800

    Fixed?

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.
 const { Client, Collection } = require("discord.js");
 // We also load the rest of the things we need in this file:
 const { CommandoClient, SQLiteProvider } = require('discord.js-commando');
-const client4 = new Discord.Client()
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
@@ -330,15 +329,12 @@ client2.on("message", async (message) => {
     }
   });
 client2.on('messageDelete', async (message) => {
-<<<<<<< HEAD
 if (message.channel.id === "787909827988946977") {
 return;
 }
-=======
 	if (message.channel.id === '787909827988946977') {
         return;
         };
->>>>>>> a20dfd76605236fa2e0ab0a44baf265ef4f34e17
 	// create a client to mongodb
 	const MongoClient = require('mongodb').MongoClient;
 	const client3 = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true }, {server: {poolSize: 1}});
@@ -401,15 +397,12 @@ if (!sChannel) return;
 	});
 });
 client2.on('messageUpdate', async (oldMessage, newMessage) => {
-<<<<<<< HEAD
 	if (oldMessage.channel.id === "787909827988946977") {
 return;
 }
-=======
 	if (message.channel.id === '787909827988946977') {
         return;
         };
->>>>>>> a20dfd76605236fa2e0ab0a44baf265ef4f34e17
 	// create a client to mongodb
 	const MongoClient = require('mongodb').MongoClient;
 	const client3 = new MongoClient(uri, { useNewUrlParser: true }, {server: {poolSize: 1}});
@@ -582,13 +575,10 @@ client.users.cache.get("742782250848092231").send("Hourly income given out")
 			uprofit+=5*user.sodaMachine + 5
 			uprofit+=10*user.toppingBar + 10
 			uprofit+=15*user.playPlace + 15
-<<<<<<< HEAD
     user.pizzaTokens += uprofit;
-    user.reviewScore = user.menu[Math.floor(Math.random() * user.menu.length)].production
-=======
+    if(user.menu) user.reviewScore = user.menu[Math.floor(Math.random() * user.menu.length)].production
       user.pizzaTokens += uprofit;
       user.reviewScore = user.menu[Math.floor(Math.random() * user.menu.length)].production / 10
->>>>>>> a20dfd76605236fa2e0ab0a44baf265ef4f34e17
     await user.save()
   })
   await (await Advertiser.find()).forEach(async advertiser => {
@@ -625,58 +615,4 @@ client.users.cache.get("742782250848092231").send("Hourly income given out")
 
   lastMessage.edit({embed: leader});
   });
-<<<<<<< HEAD
-  const pizzas = Math.round(Math.random() * 100)
-=======
-  const pizzas= Math.round(Math.random() * 100)
->>>>>>> a20dfd76605236fa2e0ab0a44baf265ef4f34e17
-  client4.channels.cache.get("781214583393615903").send(`Hey! I want to buy ${pizzas} pizzas costing ${pizzas * 12} PizzaTokens! I will buy from the person who gives me the highest discount! Auction time!`).then(botMessage => {
-    const discount = {user:null, money:0};
-    const filter = m => !isNaN(m.content)
-    const collector = botMessage.channel.createMessageCollector(filter, { time:120000 })
-
-    collector.on("collect", async m => {
-        const seller = await Seller.findOne({discord_id:m.author.id})
-        console.log(Number(m.content), m.author.id, seller)
-        if(!await Seller.findOne({discord_id:m.author.id})){
-            m.reply("You are not a seller!")
-        }
-        else if(Number(m.content)<=discount.money){
-            m.reply("You must bid higher than "+discount.money+"!")
-        }
-        else if(Number(m.content)>seller.pizzaTokens){
-            m.reply("You don't have more than "+m.content+" PizzaTokens!")
-        }
-        else if(discount.user===m.author.id){
-            m.reply("You already have the highest bid!")
-        }
-        else if(Number(m.content)>=pizzas * 12){
-            m.reply("You must bid lower than "+pizzas * 12+"!")
-        }
-        else{
-            discount.money = Number(m.content)
-            discount.user = m.author.id
-            m.reply("The highest bid is now "+discount.money+" from "+client4.users.cache.get(discount.user).tag+"!")
-        }
-    })
-
-    collector.on("end", async () => {
-        if(discount.user===null){
-            client4.channels.cache.get("781214583393615903").send(`Looks like there were no bidders, that is quite sad.`)
-        }
-        else{
-            client4.channels.cache.get("781214583393615903").send(`The auction is over! The winner was ${client4.users.cache.get(discount.user).tag} selling ${pizzas} pizzas with a ${discount.money} discount earning ${pizzas * 12 - discount.money}!`)
-            const seller = await Seller.findOne({discord_id:discount.user});
-            seller.pizzaTokens += pizzas * 12 - discount.money;
-            await seller.save()
-        }
-    })
-<<<<<<< HEAD
-})
-=======
-})  
->>>>>>> a20dfd76605236fa2e0ab0a44baf265ef4f34e17
-}, 3600000)
-
-
-client4.login(require("./config").ptoken)
+});
